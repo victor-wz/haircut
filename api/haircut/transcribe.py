c104ce9b6db5784a11570d5a42983c522e551075
Big Andy <60822006+andrewlkd@users.noreply.github.com>
2023-11-11 19:46:10 +0000

    Refactor haircut

--- api/haircut/transcribe.py
+++ api/haircut/transcribe.py
@@ -1,12 +1,13 @@
 from openai import OpenAI
-API_KEY = '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'
+API_KEY = "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"
 
 class Transcriber:
     
 	def __init__(self):
-		self._client = OpenAI(API_KEY)
+		self._client = OpenAI(api_key=API_KEY)
 	
 	def transcribe(self, audio_file):
+		audio_file= open(audio_file) # modify file 
 		transcript = self._client.audio.transcriptions.create(
 			model="whisper-1", 
 			file=audio_file
