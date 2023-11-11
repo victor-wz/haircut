ce09a112aa7765e76effba0edecfb8902cab805e
Tom Cobley <43605582+tomcobley@users.noreply.github.com>
2023-11-11 20:03:48 +0000

    some frontend woooo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import ChatBotApp from './components/ChatBotApp';
+import Recorder from './components/Recorder';
 
 function App() {
-  const [data, setData] = useState({});
+  // const [data, setData] = useState({});
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/data')
-      .then(response => setData(response.data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  // useEffect(() => {
+  //   axios.get('http://localhost:5000/api/data')
+  //     .then(response => setData(response.data))
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React App with Flask Backend</h1>
-        <p>Data from Flask API: {JSON.stringify(data)}</p>
-      </header>
+      <ChatBotApp />
+      <Recorder />
     </div>
   );
 }
