afc6fd5b54434d30d210ac51dbeba03e00a7ec19
Big Andy <60822006+andrewlkd@users.noreply.github.com>
2023-11-11 16:17:08 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React App with Flask Backend</h1>
+        <p>Data from Flask API: {JSON.stringify(data)}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
