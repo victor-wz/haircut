eabbf9b2a6a6027945ba9bfd9bcd1116dd64aed7
Big Andy <60822006+andrewlkd@users.noreply.github.com>
2023-11-12 06:31:21 +0000

    Uncomment toms gimping comments

--- api/app.py
+++ api/app.py
@@ -20,7 +20,7 @@ def mock_streaming(text):
     # Beause openai are gimps.
     for word in text.split(" "):
         socketio.emit('update_output', {'partial_result': word})
-        time.sleep(0.1)
+        time.sleep(0.15)
 
 @app.route('/api/patient/text_payload', methods=['POST'])
 def text_payload():
@@ -28,7 +28,7 @@ def text_payload():
     patient_id = data['patient_id']
     text = data['text']
     ag_res = oai_agent.process_text_payload(int(patient_id), text)
-    # mock_streaming(ag_res)
+    mock_streaming(ag_res)
     return jsonify(ag_res)
 
 @app.route('/api/patient/audio_payload', methods=['POST'])
@@ -39,7 +39,7 @@ def audio_payload():
     patient_id = request.form.get('patient_id')
     transcript = oai_transcriber.transcribe(audio_file)
     ag_res = oai_agent.process_text_payload(int(patient_id), transcript)
-    # mock_streaming(ag_res)
+    mock_streaming(ag_res)
     return jsonify(ag_res)
 
 if __name__ == '__main__':
